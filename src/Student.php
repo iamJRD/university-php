d07a9abcdfeb57f6655dbf8dfdbd4cdd382efb7a
jared beckler and josh overly <joshoverly@yahoo.com>
2016-03-01 09:02:41 -0800

    add passing tests for student getters

--- src/Student.php
+++ src/Student.php
@@ -1,6 +1,31 @@
 <?php
     class Student
     {
-        
+        private $name;
+        private $enrollment;
+        private $id;
+
+        function __construct($name, $enrollment, $id=null)
+        {
+            $this->name = $name;
+            $this->enrollment = $enrollment;
+            $this->id = $id;
+        }
+        function setName($new_name)
+        {
+            $this->name = $new_name;
+        }
+        function getName()
+        {
+            return $this->name;
+        }
+        function setEnrollment($new_enrollment)
+        {
+            $this->enrollment = $new_enrollment;
+        }
+        function getEnrollment()
+        {
+            return $this->enrollment;
+        }
     }
 ?>
--- tests/StudentTest.php
+++ tests/StudentTest.php
@@ -7,13 +7,39 @@
 
     require_once "src/Student.php";
 
-    $server = 'mysql:host=localhost;dbname=to_do_test';
+    $server = 'mysql:host=localhost;dbname=university_test';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
     class StudentTest extends PHPUnit_Framework_TestCase
     {
-        
+        function testGetName()
+        {
+            //Arrange
+            $name = "Joe";
+            $enrollment = "2016-03-01";
+            $test_student = new Student($name, $enrollment);
+
+            //Act
+            $result = $test_student->getName();
+
+            //Assert
+            $this->assertEquals($name, $result);
+        }
+
+        function testGetEnrollment()
+        {
+            // Arrange
+            $name = "Joe";
+            $enrollment = "2016-03-01";
+            $test_student = new Student($name, $enrollment);
+
+            // Act
+            $result = $test_student->getEnrollment();
+
+            // Assert
+            $this->assertEquals($enrollment, $result);
+        }
     }
 ?>
