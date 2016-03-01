e020ec00659fd5902c96f8b35645dcbedb6b38a5
jared beckler and josh overly <jared.beckler@gmail.com>
2016-03-01 11:46:18 -0800

    add silex routes and twig templates for homepage and student list page

--- app/app.php
+++ app/app.php
@@ -1,11 +1,11 @@
 <?php
     require_once __DIR__ .'/../vendor/autoload.php';
-    require_once __DIR__.'/../src/Task.php';
-    require_once __DIR__.'/../src/Category.php';
+    require_once __DIR__.'/../src/Student.php';
+    require_once __DIR__.'/../src/Course.php';
 
     $app = new Silex\Application();
 
-    $server = 'mysql:host=localhost;dbname=to_do';
+    $server = 'mysql:host=localhost;dbname=university';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
@@ -16,5 +16,38 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
-    
+// INDEX PAGE
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/students", function() use ($app) {
+        return $app['twig']->render('students.html.twig', array('students' => Student::getAll()));
+    });
+
+    $app->get("/courses", function() use ($app) {
+        return $app['twig']->render('courses.html.twig', array('courses' => Course::getAll()));
+    });
+
+    $app->post("/delete_all", function() use ($app) {
+        Course::deleteAll();
+        Student::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
+// STUDENT PAGE
+    $app->post("/add_student", function() use ($app) {
+        $name = $_POST['name'];
+        $enrollment = $_POST['enrollment'];
+        $id = null;
+        $new_student = new Student($name, $enrollment, $id);
+        $new_student->save();
+        return $app['twig']->render('students.html.twig', array('students' => Student::getAll()));
+    });
+
+    $app->post("/delete_all_students", function() use ($app) {
+        Student::deleteAll();
+        return $app['twig']->render('students.html.twig', array('students' => Student::getAll()));
+    });
+
+    return $app;
 ?>
